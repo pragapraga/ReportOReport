1b58a4d5f5f63ddc1a4d0d216096b38905116028
Pragadeeswaran <Pragadees24@gmail.com>
2020-12-10 18:19:40 +0530

    fixing some html css

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
 
 function CapturePng() {
+    /*const uiBranch = document.getElementById('branch').value;
+    document.getElementById("branchVal").innerHTML = uiBranch;
+    document.getElementById("branch").style.display = "none";
+
+    const beBranch = document.getElementById('beBranch').value;
+    document.getElementById("beBranchVal").innerHTML = beBranch;
+    document.getElementById("beBranch").style.display = "none";*/
+
     const node = document.getElementById("test-report");
     domtoimage
         .toPng(node, { bgcolor: "#FFFFFF" })
